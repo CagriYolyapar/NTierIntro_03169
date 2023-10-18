5146b76636d16c3663fc1d70661bef8490d145ce
cagri.yolyapar <cagri.yolyapar@gmail.com>
2023-10-18 14:24:19 +0300

    Add project files.

---
+++ Project.ENTITIES/Models/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Models
+{
+    public class AppUser:BaseEntity
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        //Relational Properties
+        public virtual AppUserProfile MyProperty { get; set; }
+    }
+}
